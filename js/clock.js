6325b4df4e02143f977550b0115ab1a9ff5fccd5
josxsuen <josephinersuen@gmail.com>
2015-01-26 18:19:42 -0800

    Added alarms (no persistent data)

--- js/clock.js
+++ js/clock.js
@@ -53,3 +53,54 @@ function getTemp()
         alert("Geolocation is not supported.");
     }
 }
+
+function pad(num, width)
+{
+    var num = num.toString();
+    while (num.length < width) {
+        num = "0" + num;
+    }
+    return num;
+}
+
+function initTimeContainer()
+{
+    for (var h=1; h<=12; h++) {
+        $("#hours").append("<option>" + pad(h, 2) + "</option>");
+    }
+
+    for (var m=0; m<60; m++) {
+        $("#mins").append("<option>" + pad(m, 2) + "</option>");
+    }
+}
+
+function showAlarmPopup()
+{
+    $("#mask, #popup").removeClass("hide");
+}
+
+function hideAlarmPopup()
+{
+    $("#mask, #popup").addClass("hide");
+}
+
+function insertAlarm(hours, mins, ampm, alarmName)
+{
+    var newAlarm = $("<div>").addClass("flexable");
+
+    newAlarm.append($("<div>").addClass("name").html(alarmName));
+    newAlarm.append($("<div>").addClass("time").html(hours + ":" + mins + " " + ampm));
+
+    $("#alarms").append(newAlarm);
+}
+
+function addAlarm()
+{
+    var hours = $("#hours option:selected").text();
+    var mins  = $("#mins option:selected").text();
+    var ampm  = $("#ampm option:selected").text();
+    var alarmName = $("#alarmName").val();
+
+    insertAlarm(hours, mins, ampm, alarmName);
+    hideAlarmPopup();
+}
