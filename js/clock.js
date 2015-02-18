689647c5bc3783c8133fe2ef77410c3d9e709cc1
josxsuen <josephinersuen@gmail.com>
2015-02-17 17:15:04 -0800

    Execute signout functionality for any authResult error

--- js/clock.js
+++ js/clock.js
@@ -218,7 +218,7 @@ function signout() {
 }
 
 function signinCallback(authResult) {
-    if (authResult['status']['signed_in']) {
+    if (authResult.status.signed_in) {
         gapi.client.load('plus','v1').then(function() {
             gapi.client.plus.people.get({
                 'userId': 'me'
@@ -227,15 +227,13 @@ function signinCallback(authResult) {
             });
         });
     }
-    else if (authResult['error'] === 'user_signed_out') {
-        signout();
-    }
     else {
         // Update the app to reflect a signed out user
         // Possible error values:
         //   "user_signed_out" - User is signed-out
         //   "access_denied" - User denied access to your app
         //   "immediate_failed" - Could not automatically log in the user
-        console.log('Sign-in state: ' + authResult['error']);
+        console.log('Sign-in state: ' + authResult.error);
+        signout();
     }
 }
